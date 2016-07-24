27262e1b7fbbdadd107840ae3b269eec225ffb35
Oskar <oskarradonkimball@gmail.com>
2016-07-24 11:06:07 -0700

    Refactoring javaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,47 @@
 $(function() {
 
+	var animationDuration = 1;
+	var slider = $('.slider');
+	function sliderAbout() {
+		TweenMax.to(slider, animationDuration, {backgroundColor: '#FFEF4F', width: '71px', marginLeft:0});
+	}
+
+	function sliderWork() {
+		TweenMax.to(slider, animationDuration, {backgroundColor: '#FF4747', width: '63px', marginLeft:91});
+	}
+
+	function sliderContact() {
+		TweenMax.to(slider, animationDuration, {backgroundColor: '#7AD1D1', width: '50px', marginLeft:200});
+	}
+
 	// Set nav slider
 	function setSlider() {
 		var pathname = window.location.pathname;
 		if (pathname === 'about.html') {
-			$('.slider').css({'margin-left': '0px'});
+			sliderAbout();
 		} else if (pathname === 'contact.html') {
-			$('.slider').css({'margin-left': '200px'});
+			sliderContact();
 		} else {
-			$('.slider').css({'margin-left': '50px'});
+			sliderWork();
 		}
 	}
 	setSlider();
-		
-
+		 
 	// Animate nav slider
 
 	$('.about').hover(function() {
-		TweenMax.to($('.slider'), 1, {backgroundColor: '#FFEF4F', width: '60px', marginLeft:0});
-	}, setSlider())
+		sliderAbout();
+	}, function() {
+		setSlider();
+	})
 	$('.work').hover(function() {
-		TweenMax.to($('.slider'), 1, {backgroundColor: '#FF4747', width: '50px', marginLeft:100});
-	}, setSlider())
+		sliderWork();
+	}, function() {
+		setSlider();
+	})
 	$('.contact').hover(function() {
-		TweenMax.to($('.slider'), 1, {backgroundColor: '#7AD1D1', width: '50px', marginLeft:200});
-	}, setSlider())
+		sliderContact();
+	}, function() {
+		setSlider();
+	})
 })
