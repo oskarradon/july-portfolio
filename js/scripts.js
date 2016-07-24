26a9fc23d8886d7ed7a2306b49d1d5e94b3ca407
Oskar <oskarradonkimball@gmail.com>
2016-07-24 11:22:50 -0700

    Add comments to js

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,22 @@ $(function() {
 
 	var animationDuration = 1;
 	var slider = $('.slider');
+	var easing = Power4.easeOut;
+
+	// Slider animations
 	function sliderAbout() {
-		TweenMax.to(slider, animationDuration, {backgroundColor: '#FFEF4F', width: '71px', marginLeft:0});
+		TweenMax.to(slider, animationDuration, {ease: easing, backgroundColor: '#FFEF4F', width: '72px', marginLeft:-1});
 	}
 
 	function sliderWork() {
-		TweenMax.to(slider, animationDuration, {backgroundColor: '#FF4747', width: '63px', marginLeft:91});
+		TweenMax.to(slider, animationDuration, {ease: easing, backgroundColor: '#FF4747', width: '64px', marginLeft:91});
 	}
 
 	function sliderContact() {
-		TweenMax.to(slider, animationDuration, {backgroundColor: '#7AD1D1', width: '50px', marginLeft:200});
+		TweenMax.to(slider, animationDuration, {ease: easing, backgroundColor: '#7AD1D1', width: '97px', marginLeft:174});
 	}
 
-	// Set nav slider
+	// Animate nav slider based on current page
 	function setSlider() {
 		var pathname = window.location.pathname;
 		if (pathname === 'about.html') {
@@ -25,10 +28,11 @@ $(function() {
 			sliderWork();
 		}
 	}
+	
+	// Animate slider on page load
 	setSlider();
 		 
-	// Animate nav slider
-
+	// Animate nav slider on hover
 	$('.about').hover(function() {
 		sliderAbout();
 	}, function() {
