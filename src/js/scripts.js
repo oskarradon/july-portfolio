ef9bfbc8dce5baa47ed1f59d605447b2ab0dc268
Oskar <oskarradonkimball@gmail.com>
2016-08-01 14:13:48 -0700

    Remove jade filter from js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,3 @@
-require('jade').filters.escape = function( block ) {
-    return block
-        .replace( /&/g, '&amp;'  )
-        .replace( /</g, '&lt;'   )
-        .replace( />/g, '&gt;'   )
-        .replace( /"/g, '&quot;' )
-        .replace( /#/g, '&#35;'  )
-        .replace( /\\/g, '\\\\'  )
-        .replace( /\n/g, '\\n'   );
-}
-
 $(function() {
 
 	var animationDuration = 1;
