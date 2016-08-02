a23d82a310f510ac6ae8e7dcc8de37803b8dfc31
Oskar <oskarradonkimball@gmail.com>
2016-08-01 23:35:58 -0700

    Add responsive Slick initialize function

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -52,4 +52,39 @@ $(function() {
 	}, function() {
 		setSlider();
 	})
+
+	// Initialize Slick carousel
+	$('.slick').slick({
+		dots: true,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+			{
+				breakpoint: 1088,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 800,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 400,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+	
 })
