cdb8d16f3bf7bbaad1629acf33c8c85b508a4059
Oskar <oskarradonkimball@gmail.com>
2016-08-02 00:04:46 -0700

    Final Slick changes lol

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -58,19 +58,12 @@ $(function() {
 		dots: true,
 		infinite: false,
 		arrows: false,
+		autoplay: false,
+		autoplaySpeed: 2000,
 		speed: 300,
-		slidesToShow: 4,
-		slidesToScroll: 4,
+		slidesToShow: 3,
+		slidesToScroll: 3,
 		responsive: [
-			{
-				breakpoint: 1088,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-					infinite: true,
-					dots: true
-				}
-			},
 			{
 				breakpoint: 800,
 				settings: {					
