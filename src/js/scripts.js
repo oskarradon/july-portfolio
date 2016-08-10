9e95462fd83625fafdd0921328a96ae59a81f938
Oskar <oskarradonkimball@gmail.com>
2016-08-09 22:51:00 -0700

    Un-do further refactoring to get it working again

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -41,17 +41,11 @@ setSlider();
 var navElements = ['.about', '.work', '.contact'];
 var sliderFunctions = [sliderAbout, sliderWork, sliderContact];
 
-var sliderEvents = [
-	{element: '.about', funct: sliderAbout},
-	{element: '.work', funct: sliderWork},
-	{element: '.contact', funct: sliderContact}
-];
-
-sliderEvents.forEach(function(event, index) {
-	document.querySelector(event.element).addEventListener('mouseenter', function() {
-		event.funct[index]();
+navElements.forEach(function(element, index) {
+	document.querySelector(element).addEventListener('mouseenter', function() {
+		sliderFunctions[index]();
 	})
-	document.querySelector(event.element).addEventListener('mouseleave', function() {
+	document.querySelector(element).addEventListener('mouseleave', function() {
 		setSlider();
 	})
 })
