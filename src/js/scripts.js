4e4eef149b5256e3359a1ec6f44312819abeb679
Oskar <oskarradonkimball@gmail.com>
2016-08-23 21:59:40 -0700

    Change slider color, add chart

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,12 +5,12 @@ var slider = document.getElementsByClassName('slider');
 var easing = Power4.easeOut;
 
 var colorRed = '#FF4747';
-var colorYellow = '#FFEF4F';
+var colorGreen = '#79D879';
 var colorBlue = '#7AD1D1';
 
 // Slider animations
 function sliderAbout() {
-	TweenMax.to(slider, animationDuration, {ease: easing, backgroundColor: colorYellow, width: '72px', marginLeft:-1});
+	TweenMax.to(slider, animationDuration, {ease: easing, backgroundColor: colorGreen, width: '72px', marginLeft:-1});
 }
 
 function sliderWork() {
@@ -79,3 +79,41 @@ if (document.querySelector('.slick')) {
 		]
 	});
 }
+
+var ctx = document.getElementById("skillsChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
